f1f9b184a4f5a93561d8270e74aa88c8591ee74e
lepidotteri <vellocatus.veil@gmail.com>
2021-02-26 16:20:36 +0100

    Completed 'Building on Optimism' tutorial

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import { HardhatUserConfig } from 'hardhat/types'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 
+import '@eth-optimism/plugins/hardhat/compiler'
+import '@eth-optimism/plugins/hardhat/ethers'   // Now just add this one!
+
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
 };
